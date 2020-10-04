afbbc084af1d493c2caf4c4d8e496df71b191ab7
Ivo Saavedra <ivo_saavedra@outlook.com>
2020-10-04 20:17:06 +0100

    uncomented initial verification

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -22,12 +22,12 @@ int main(int argc, char** argv)
     char buf[255];
     int i, sum = 0, speed = 0;
     
-    // if ( (argc < 2) || 
-  	//     //  ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	//     //   (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-    //   printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-    //   exit(1);
-    // }
+    if ( (argc < 2) || 
+  	    //  ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	    //   (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
 
 
   /*
