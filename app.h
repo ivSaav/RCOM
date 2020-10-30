5364a7f788df1b4329cc517df682ac8c5ef3646e
BernardoRamalho <bernardoramalho99@gmail.com>
2020-10-30 18:55:25 +0000

    Added comments to app.h

--- app.h
+++ app.h
@@ -23,7 +23,50 @@ typedef struct {
     char *filename;
 } App;
 
+/**
+ * Converts and integer into hexadecimal
+ * 
+ * unsigned char *ret :- variable to store the hexadecimal value
+ * int value :- value of the integer to be converted
+ * 
+ * returns the length of the hexadecimal
+ */
+int intToHexa(unsigned char *ret, int value);
+
+/**
+ * Sends a Control Frame through the file descriptor
+ * described in struct App as fd
+ * 
+ * using char controlFlag :- controlFlag representing if
+ * it's the begging or end of the connection
+ * 
+ * returns 0 if it was sent successfully
+ */
 int sendControlFrame(unsigned char controlFlag);
+
+/**
+ * Receives a Control Frame through the file descriptor
+ * described in struct App as fd
+ * 
+ * using char controlFlag :- control flag to be compared to
+ * the one received
+ * 
+ * returns 0 if it was sent successfully
+ */
 int receiveControlFrame(unsigned char controlFlag);
 
+/**
+ * Sends a Data Frames through the file descriptor
+ * described in struct App as fd
+ * 
+ * returns 0 if it was sent successfully
+ */
 int sendDataFrames();
+
+/**
+ * Sends a Data Frames through the file descriptor
+ * described in struct App as fd
+ * 
+ * returns 0 if it was sent successfully
+ */
+int receiveDataFrames();
